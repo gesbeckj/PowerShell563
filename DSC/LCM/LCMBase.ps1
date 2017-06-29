16c3eebfb97dbbec70ed8a34bf48f629a25b1e82
James Gesbeck <james@gesbeck.com>
2017-06-29 16:33:42 -0500

    LCM Base Config

---
+++ DSC/LCM/LCMBase.ps1
@@ -0,0 +1,36 @@
+﻿[DSCLocalConfigurationManager()]
+Configuration LCM {
+    param
+    (
+        [Parameter(Mandatory=$True)]
+        [PSCredential]$RegistrationKey
+    )
+    Node localhost {
+        Settings {
+            ConfigurationMode = 'ApplyAndAutoCorrect'
+            RefreshMode = 'Pull'
+        }
+        ConfigurationRepositoryWeb DSCPullServer {
+            ServerURL = 'https://pull.gesbeck.tk:8080/PsDscPullserver.svc'
+            AllowUnsecureConnection = $false
+            RegistrationKey = $RegistrationKey.GetNetworkCredential().Password
+            ConfigurationNames = @('BaseOSConfig', 'EnableRDP')
+        }
+        ResourceRepositoryWeb PullServerModules {
+            ServerURL = 'https://pull.gesbeck.tk:8080/PsDscPullserver.svc'
+            AllowUnsecureConnection = $false
+            RegistrationKey = $RegistrationKey.GetNetworkCredential().Password
+        }
+        ReportServerWeb PullServerReporting
+        {
+            ServerUrl = $RegistrationUrl
+            RegistrationKey = $RegistrationKey.GetNetworkCredential().Password
+        }
+        PartialConfiguration BaseOSConfig {
+            ConfigurationSource = "[ConfigurationRepositoryWeb]DSCPullServer"
+        }
+        PartialConfiguration EnableRDP {
+            ConfigurationSource = "[ConfigurationRepositoryWeb]DSCPullServer"
+        }
+    }
+}
