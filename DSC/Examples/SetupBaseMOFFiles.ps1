977a569c220de32c0b9de7fb3b0326722f288dc4
James Gesbeck <james@gesbeck.com>
2017-07-24 16:24:25 -0500

    Rename RDP File

---
+++ DSC/Examples/SetupBaseMOFFiles.ps1
@@ -0,0 +1,7 @@
+﻿$path = $pwd.ToString()
+& 'C:\Users\james.gesbeck\Documents\GitHub\PowerShell563\DSC\Windows Server\BaseOSConfig.ps1' -Path $path
+Rename-Item -Path ($path + '\localhost.mof') -NewName 'BaseOSConfig.mof'
+New-DscChecksum -Path ($path + '\BaseOSConfig.mof')
+& 'C:\Users\james.gesbeck\Documents\GitHub\PowerShell563\DSC\Windows Server\DSCEnableRDP.ps1' -path $path
+Rename-Item -Path ($path + '\localhost.mof') -NewName 'EnableRDP.mof'
+New-DscChecksum -Path ($path + '\EnableRDP.mof')
