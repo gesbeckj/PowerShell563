fc333aa4ee28fde5f08131dc7a8db86f3ff9265e
James Gesbeck <james@gesbeck.com>
2018-03-09 14:24:44 -0600

    MSOL Updates

---
+++ Scripts/In Development/Untitled2.ps1
@@ -0,0 +1,11 @@
+﻿Add-PSSnapin Microsoft.Exchange.Management.PowerShell.SnapIn;
+$username = read-host "Please enter User's username"
+$uid = $username + "@gesbeck.org"
+Set-ADUser $uid -Clear homemdb, homemta, msExchHomeServerName, msExchPoliciesExcluded
+Set-ADUser $uid -Add @{msExchRemoteRecipientType="6"}
+Set-ADUser $uid -Add @{mailNickname="$uid"}
+Set-ADUser $uid -Add @{msExchProvisioningFlags="0"}
+Set-ADUser $uid -Add @{msExchModerationFlags="6"}
+Set-ADUser $uid -Add @{msExchAddressBookFlags="1"}
+Set-ADUser $uid -Replace @{msExchRecipientDisplayType="-2147483642"}
+Set-ADUser $uid -Replace @{msExchRecipientTypeDetails="2147483648"}
---
+++ Scripts/In Development/Untitled3.ps1
@@ -0,0 +1,9 @@
+﻿$Tenants = Get-MsolPartnerContract -All | Select-Object TenantId
+$role = Get-MsolRole -RoleName "Company Administrator"
+$AdminInfo = Get-MsolRoleMember -RoleObjectId $role.ObjectId | Where-Object {$_.DisplayName -ne "Abe Aberdean"} | Where-Object {$_.EmailAddress -notlike "administrator@*"} |  Select DisplayName, EmailAddress
+foreach ($company in $Tenants)
+{
+    
+   # write-host $company
+    $AdminInfo += Get-MsolRoleMember -RoleObjectId $role.ObjectId -TenantId $company.TenantId | Where-Object {$_.DisplayName -ne "Abe Aberdean"} | Where-Object {$_.EmailAddress -notlike "administrator@*"} | Where-Object {$_.EmailAddress -notlike "admin@*"} | Select DisplayName, EmailAddress
+}
