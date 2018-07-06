88d4c6485fa1b13452e96c471f1597d17fa80de7
James Gesbeck <james@gesbeck.com>
2018-07-06 14:04:54 -0500

    Added Create-EventEntry

---
+++ Scripts/Windows Server Configuration/Create-EventEntry.ps1
@@ -0,0 +1,23 @@
+<#
+.SYNOPSIS
+This creates an error event entry in the application log with the specified source and message. 
+.DESCRIPTION
+This creates an error event entry in the application log with the specified source and message. 
+.EXAMPLE
+Create-EventEntry -Source "Test" -Message "This is a test application error message"
+.NOTES
+Must run as Local Administrator
+#>
+[CmdletBinding()]
+
+param (
+    [Parameter(Mandatory)]
+    [ValidateNotNullOrEmpty()]
+    [string]$Source,
+
+    [Parameter(Mandatory)]
+    [ValidateNotNullOrEmpty()]
+    [string]$Message
+)
+New-EventLog -LogName Application -Source $source -ErrorAction Continue
+Write-EventLog -LogName Application -Source $Source -EventId 66 -EntryType Error -Message $Message
