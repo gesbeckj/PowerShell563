e7dcfc2669f591ac5ae14e05658bd8a1ee37641c
gesbeckj <james@gesbeck.com>
2019-09-15 14:26:19 -0500

    Update Install-WindowsAdminCenter.ps1

--- Scripts/Aberdean/Install-WindowsAdminCenter.ps1
+++ Scripts/Aberdean/Install-WindowsAdminCenter.ps1
@@ -30,8 +30,9 @@ Process
         Write-Error "An unknown error occuring. The installer is not at $DownloadDestination as expected"
         return
     }
-    Write-Verbose "Starting Windows Admin Center Install. Logging install to C:\log.txt"
-    Start-Process msiexec.exe -Wait -ArgumentList "/i $DownloadDestination /qn /L*v c:\log.txt SME_PORT=$WindowsAdminCenterHTTPSPort SSL_CERTIFICATE_OPTION=generate"
+    Write-Verbose "Windows Admin Center will listen on port $WindowsAdminCenterHTTPSPort"
+    Write-Verbose "Starting Windows Admin Center Install. Logging install to $env:TEMP\log.txt"
+    Start-Process msiexec.exe -Wait -ArgumentList "/i $DownloadDestination /qn /L*v $env:TEMP\log.txt SME_PORT=$WindowsAdminCenterHTTPSPort SSL_CERTIFICATE_OPTION=generate"
     Remove-Item $DownloadDestination
-    Remove-Item 'c:\log.txt'
+    Write-Verbose "Windows Admin Installer Completed. Installation log file is at $env:TEMP\log.txt"
 }
