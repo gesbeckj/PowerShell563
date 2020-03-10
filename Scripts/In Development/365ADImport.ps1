4e22b341ae1ae2e56b5e3d9922086f98f2bac0c9
James Gesbeck <james@gesbeck.com>
2020-03-10 14:35:28 -0500

    A

--- Scripts/In Development/365ADImport.ps1
+++ Scripts/In Development/365ADImport.ps1
@@ -2,22 +2,9 @@ $file = "C:\Users\Administrator\desktop\sw.xml"
 $userlist = Import-Clixml $file
 foreach ($user in $userlist)
 {
-Add-PSSnapin Microsoft.Exchange.Management.PowerShell.SnapIn;
 $uid = $user.Identity
-$pmail = $uid+"@seventhwave.org"
-Set-ADUser $uid -Clear homemdb, homemta, msExchHomeServerName, msExchPoliciesExcluded
-Set-ADUser $uid -Add @{msExchRemoteRecipientType="4"}
-Set-ADUser $uid -Add @{mailNickname="$uid"}
-Set-ADUser $uid -Add @{msExchProvisioningFlags="0"}
-Set-ADUser $uid -Add @{msExchModerationFlags="6"}
-Set-ADUser $uid -Add @{msExchAddressBookFlags="1"}
-Set-ADUser $uid -Replace @{targetaddress="$pmail"}
-Set-ADUser $uid -Replace @{msExchRecipientDisplayType="-2147483642"}
-Set-ADUser $uid -Replace @{msExchRecipientTypeDetails="2147483648"}
-Set-ADUser $uid -Replace @{msExchRecipientTypeDetails="2147483648"}
 foreach ($emailAddr in $user.proxyAddresses)
 {
     Set-AdUser $uid -Add @{proxyAddresses=$emailAddr}
 }
-Set-AdUser $uid -Replace @{mail=$user.PrimarySmtpAddress.ToString()}
 }
--- Scripts/In Development/ConfigureWSB.ps1
+++ Scripts/In Development/ConfigureWSB.ps1
@@ -1,8 +1,9 @@
 $BackupPolicy = New-WBPolicy
-$VM = Get-WBVirtualMachine | Where-Object {$_.VMName -eq "MCF-OFC-Mitchell" -or $_.VMName -eq "MCF-OFC-TS1"}
+$VM = Get-WBVirtualMachine | Where-Object {$_.VMName -eq "MCF-OFC-Mitchell" -or $_.VMName -eq "MCF-OFC-TS1" -or $_.VMName -eq "MCF-OFC-File" -or $_.VMName -eq "Permar"}
+$VM = Get-WBVirtualMachine | Where-Object {$_.VMName -eq "MCF-MFG-PDM" -or $_.VMName -eq "MCF-FabSuite" -or $_.VMName -eq "Accrotimeserver"}
 Add-WBVirtualMachine -Policy $BackupPolicy -VirtualMachine $VM
-$Disk = Get-WBDisk | Where-Object {$_.DiskNumber -eq 7}
-$Target = New-WBBackupTarget -Disk $Disk -PreserveExistingBackups $true
+$Disk = Get-WBDisk | Where-Object {$_.DiskNumber -eq 2}
+$Target = New-WBBackupTarget -Disk $Disk -PreserveExistingBackups $false
 Add-WBBackupTarget -policy $BackupPolicy -target $Target  -force
 $sch2 = [datetime]"01/31/2011 21:00:00"
 Set-WBSchedule -policy $BackupPolicy -schedule $sch2
